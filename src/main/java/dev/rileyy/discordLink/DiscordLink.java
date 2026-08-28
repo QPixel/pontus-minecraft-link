15611922d056d139b1cef2e9a55e9915e8881a23
Riley Smith <riley@rileysmith.me>
2026-08-27 17:53:20 -0700

    setup files, grpc

---
+++ src/main/java/dev/rileyy/discordLink/DiscordLink.java
@@ -0,0 +1,10 @@
+package dev.rileyy.discordLink;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DiscordLink implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
